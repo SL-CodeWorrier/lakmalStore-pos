995019275c584dbdb49904fbdb0ebba7b9d3c54d
Chathura Aththanayaka <chathuathnayaka@gmail.com>
2023-12-19 11:16:46 +0530

    create the customer ui

---
+++ src/com/devstack/pos/controller/CustomerFormController.java
@@ -0,0 +1,26 @@
+package com.devstack.pos.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CustomerFormController {
+
+    public AnchorPane context;
+    public JFXTextField txtEmail;
+    public JFXTextField txtName;
+    public JFXTextField txtContact;
+    public JFXTextField txtSalary;
+    public JFXButton btnSaveUpdate;
+    public TextField txtSearch;
+    public TableView tbl;
+    public TableColumn colId;
+    public TableColumn colEmail;
+    public TableColumn colName;
+    public TableColumn colContact;
+    public TableColumn colSalary;
+    public TableColumn colOperate;
+}
